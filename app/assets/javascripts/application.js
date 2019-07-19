ba9a7a46e5ed69873c379367175fba586d6278e5
kirilldkozlov <kirill.d.kozlov@gmail.com>
2019-07-19 00:08:56 -0400

    Add search js, update index page, new seeds and add query to controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require search
 //= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+waterloo = Address.create(street_num: 1, street_name: "University St", postal_code: "N1N AAA", city: "Waterloo",  province: "Ontario")
+toronto = Address.create(street_num: 2, street_name: "Yonge St", postal_code: "M1M AAA", city: "Toronto",  province: "Ontario")
+
+venue_waterloo = Venue.create(name: "Wonderful Waterloo", cost: 100)
+venue_toronto = Venue.create(name: "Somewhere in Toronto", cost: 1000)
+
+venue_waterloo_address = VenueAddress.create(venue: venue_waterloo, address: waterloo)
+venue_toronto_address = VenueAddress.create(venue: venue_toronto, address: toronto)
